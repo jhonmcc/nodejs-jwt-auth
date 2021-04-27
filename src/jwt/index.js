f1732367519fad1bf7a0f6b41656a187f3b5ba9a
dev <dev@DESKTOP-2UE8KHN.portoseguro.brasil>
2021-04-27 16:31:14 -0300

    adicionado referencia arquivo .env, modificado para teste de jwt, obs o projeto ainda nao esta organizado

--- src/jwt/index.js
+++ src/jwt/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
+require('dotenv').config() //para injetar as variaveis de ambiente do arquivo .env
 
 const app = express()
 app.use(express.json())
@@ -11,9 +13,27 @@ app.get('/', (req, res, next) => {
 
 // simulando retorno de todos os clientes
 app.get('/clientes', (req, res, next) => {
-    res.json([{id: 1, nome: 'Jhon'}])
+    res.json([{id: 1, nome: 'Test'}])
 })
 
+// autenticacao
+app.post('/login', (req, res, next) => {
+    // teste de validacao e retorno do usuario pelo banco de dados 
+    if (req.body.user === 'test' && req.body.pass === '123'){
+        // auth ok
+        const id = 1
+        const token = jwt.sign({ id }, process.env.SECRET, {
+            expiresIn: '2m'
+        })
+        return res.json({ auth: true, token: token })
+    }
+
+    res.status(401).json({ message: 'Login ou Senha invalidos'})
+})
+
+app.route('/logout', (req, res, next) => {
+    res.json({ auth: false, token: null })
+})
 
 app.listen(3000)
 console.log('server online')
